91ab72e733fc5837ffce0fb4f6341454d316137d
Anushka Nair <anush0530@gmail.com>
2024-07-30 19:56:49 -0400

    Added files and code

---
+++ ProgrammingAssignment2.R
@@ -0,0 +1,32 @@
+
+makeCacheMatrix <- function(x = matrix()) {
+  inv <- NULL
+  set <- function(y) {
+    x <<- y
+    inv <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) inv <<- inverse
+  getInverse <- function() inv
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
+
+cacheSolve <- function(x, ...) {
+  inv <- x$getInverse()
+  if(!is.null(inv)) {
+    message("getting cached data")
+    return(inv)
+  }
+  data <- x$get()
+  inv <- solve(data, ...)
+  x$setInverse(inv)
+  inv
+}
+# Create a matrix
+ my_matrix <- matrix(c(1, 2, 3, 4), 2, 2)
+
+# Create a special "matrix" object using makeCacheMatrix
+cachedMatrix <- makeCacheMatrix(my_matrix)
